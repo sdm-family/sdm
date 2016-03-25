d0dedc23fb47288b4d9cfec93f7070a0d13fc149
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2016-03-25 13:07:14 +0900

    ネストしたComponentの子要素にComponentを複数指定できないバグを修正

--- Sdm/Component.fs
+++ Sdm/Component.fs
@@ -3,20 +3,21 @@
 type Component =
   | Heading of TextStyleGroup list * Level:int * Value:Text
   | Paragraph of TextStyleGroup list * Lines:Text list
-  | List of ListStyleGroup list * Items:Component list
+  | List of ListStyleGroup list * Items:Components list
   | Table of TableStyleGroup list * TableContents
+and Components = Component list
 and TableContents =
   | RowsTable of ColumnStyleGroup list * ColumnContents list
   | ColumnsTable of RowStyleGroup list * RowContents list
 and ColumnContents = {
   Heading: TableHeading option
-  Rows: (CellStyleGroup list * Component) list
+  Rows: (CellStyleGroup list * Components) list
 }
 and RowContents = {
   Heading: TableHeading option
-  Columns: (CellStyleGroup list * Component) list
+  Columns: (CellStyleGroup list * Components) list
 }
 and TableHeading = {
   StyleGroups: CellStyleGroup list
-  Content: Component
+  Content: Components
 }
